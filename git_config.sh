76bdbbd5ab4055045b665e82a1370b8d7f5ead3b
lucasfuriofranco@gmail.com <LucasFurioFranco>
2023-06-04 01:39:00 +0000

    git settings on startup v3

--- git_config.sh
+++ git_config.sh
@@ -4,8 +4,8 @@ user_mail="lucasfuriofranco@gmail.com"
 repo_name="brobath_docker_www"
 
 #https://stackoverflow.com/questions/18935539/authenticate-with-github-using-a-token
-git config --global user.name ${user_mail}
-git config --global user.email ${user_name}
+git config --global user.name ${user_name}
+git config --global user.email ${user_mail}
 
 git remote remove origin
 
